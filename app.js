53e021ab110cc682800ec64efcb4f41f91f6b5bb
Naresh E <nareshe64@gmail.com>
2021-04-29 20:29:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,83 @@
+var app = require('express')()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server)
+  , nStore = require('nstore')
+  , client = require('twilio')('AC045c356e751e254a63603a90b5afcfd8','604b772250435fe2fbe12cce49213c44')
+  , speakeasy = require('speakeasy');
+
+require('dotenv').config();
+
+var users = nStore.new('data/users.db', function () {
+  console.log("Loaded users.db");
+});
+
+server.listen(3000);
+
+app.get('/',function(req,res){
+  res.render('index.jade')
+});
+
+function createUser(phone_number, code, socket) {
+  users.save(phone_number, {code: code, verified: false}, function (saverr) {
+    if (saverr) { throw saverr; }
+    client.sendSms({
+        to: phone_number,
+        from: '+14158815894',
+        body: 'Your verification code is: ' + code
+    }, function(twilioerr, responseData) {
+      console.log(twilioerr,responseData);
+      if (twilioerr) { 
+        users.remove(phone_number, function(remerr) {if (remerr) { throw remerr; }});
+        socket.emit('update', {message: "Invalid phone number!"});
+      } else {
+        socket.emit('code_generated');
+      }
+    });
+  });
+}
+
+function checkVerified(socket, verified, number) {
+  if (verified == true) {
+    socket.emit('reset');
+    socket.emit('update', {message: "You have already verified " + number + "!"});
+    return true;
+  }
+  return false;
+}
+
+io.sockets.on('connection', function(socket) {
+  console.log('socket.io connected');
+  socket.on('register', function(data) {
+    var code = speakeasy.totp({key: 'abc123'});
+    users.get(data.phone_number, function (geterr, doc, key) {
+      if (geterr) {
+        createUser(data.phone_number, code, socket);
+      }
+      else if (checkVerified(socket, doc.verified, data.phone_number) == false) {
+        socket.emit('update', {message: "You have already requested a verification code for that number!"});
+        socket.emit('code_generated');
+      }
+    });
+
+  });
+
+  socket.on('verify', function(data) {
+    users.get(data.phone_number, function (geterr, doc, key) {
+      if (geterr) {
+        socket.emit('reset');
+        socket.emit('update', {message: "You have not requested a verification code for " + data.phone_number + " yet!"});
+      }
+      else if (checkVerified(socket, doc.verified, data.phone_number) == false && doc.code == parseInt(data.code)) {
+        socket.emit('verified');
+        socket.emit('update', {message: "You have successfully verified " + data.phone_number + "!"});
+        users.save(data.phone_number, {code: parseInt(data.code), verified: true}, function (saverr) { if (saverr) { throw saverr; }});
+      }
+      else {
+        socket.emit('update', {message: "Invalid verification code!"});
+      }
+    });
+
+  });
+});
+
